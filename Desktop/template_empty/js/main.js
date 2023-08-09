5494d91ac5e9b2ecfaa55449b5caa5cbab64538f
iamdumonde <martinaboumonde@gmail.com>
2023-08-09 18:36:48 +0100

    quelques modifications

--- Desktop/template_empty/js/main.js
+++ Desktop/template_empty/js/main.js
@@ -1,8 +1,21 @@
 'use strict'
 
-// JS menu-hamburger
-const menuHamburger = document.querySelector('.menu-hamburger');
-const navLinks = document.querySelector('.nav-links');
-menuHamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('mobile-menu')
-});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // JS menu-hamburger
+    const menuHamburger = document.querySelector('.menu-hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    //Footer
+    const paragraphInFooter = document.querySelector('.footer__p');
+    const body = document.querySelector('body');
+
+    menuHamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('mobile-menu')
+    });
+
+
+    if (!paragraphInFooter) {
+        body.innerHTML = ' ';
+    }
+
+})
